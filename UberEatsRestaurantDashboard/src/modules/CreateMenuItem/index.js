e49d803fd375cb61058436eb097e023b6d6fedfc
Vadim Savin <savinvadim1312@gmail.com>
2022-05-13 18:15:50 +0100

    Menu management (CRUD)

--- UberEatsRestaurantDashboard/src/modules/CreateMenuItem/index.js
+++ UberEatsRestaurantDashboard/src/modules/CreateMenuItem/index.js
@@ -1,26 +1,72 @@
-import { Form, Input, Button, Card, InputNumber } from "antd";
+import { Form, Input, Button, Card, InputNumber, message } from "antd";
+import { DataStore } from "aws-amplify";
+import { Dish } from "../../models";
+import { useRestaurantContext } from "../../contexts/RestaurantContext";
+import { useNavigate } from "react-router-dom";
 
 const { TextArea } = Input;
 
 const CreateMenuItem = () => {
+  const { restaurant } = useRestaurantContext();
+  const navigation = useNavigate();
+
+  const onFinish = ({ name, description, price }) => {
+    DataStore.save(
+      new Dish({
+        name,
+        description,
+        price,
+        restaurantID: restaurant.id,
+      })
+    );
+    message.success("Dish was created");
+    navigation("/menu");
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <Card title="New Menu Item" style={{ margin: 20 }}>
-      <Form layout="vertical" wrapperCol={{ span: 8 }}>
-        <Form.Item label="Dish Name" required>
-          <Input placeholder="Enter dish name"/>
+      <Form
+        layout="vertical"
+        wrapperCol={{ span: 8 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Dish Name"
+          name="name"
+          rules={[{ required: true }]}
+          required
+        >
+          <Input placeholder="Enter dish name" />
         </Form.Item>
-        <Form.Item label="Dish Description" required>
-          <TextArea rows={4} placeholder="Enter dish description"/>
+        <Form.Item
+          label="Dish Description"
+          name="description"
+          rules={[{ required: true }]}
+          required
+        >
+          <TextArea rows={4} placeholder="Enter dish description" />
         </Form.Item>
-        <Form.Item label="Price ($)" required>
+        <Form.Item
+          label="Price ($)"
+          name="price"
+          rules={[{ required: true }]}
+          required
+        >
           <InputNumber />
         </Form.Item>
         <Form.Item>
-          <Button type="primary">Submit</Button>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </Form.Item>
       </Form>
     </Card>
-  )
+  );
 };
 
 export default CreateMenuItem;
--- UberEatsRestaurantDashboard/src/modules/RestaurantMenu/index.js
+++ UberEatsRestaurantDashboard/src/modules/RestaurantMenu/index.js
@@ -1,8 +1,27 @@
-import { Card, Table, Button } from "antd";
-import dishes from "../../assets/data/dishes.json";
-import { Link } from 'react-router-dom'
+import { Card, Table, Button, Popconfirm } from "antd";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { DataStore } from "aws-amplify";
+import { Dish } from "../../models";
+import { useRestaurantContext } from "../../contexts/RestaurantContext";
 
 const RestaurantMenu = () => {
+  const [dishes, setDishes] = useState([]);
+  const { restaurant } = useRestaurantContext();
+
+  useEffect(() => {
+    if (restaurant?.id) {
+      DataStore.query(Dish, (c) => c.restaurantID("eq", restaurant.id)).then(
+        setDishes
+      );
+    }
+  }, [restaurant?.id]);
+
+  const deleteDish = (dish) => {
+    DataStore.delete(dish);
+    setDishes(dishes.filter((d) => d.id !== dish.id));
+  };
+
   const tableColumns = [
     {
       title: "Menu Item",
@@ -11,22 +30,32 @@ const RestaurantMenu = () => {
     },
     {
       title: "Price",
-      dataIndex: 'price',
-      key: 'price',
-      render: (price) => `${price} $` 
+      dataIndex: "price",
+      key: "price",
+      render: (price) => `${price} $`,
     },
     {
       title: "Action",
-      key: 'action',
-      render: () => <Button danger>Remove</Button>
-    }
+      key: "action",
+      render: (_, item) => (
+        <Popconfirm
+          placement="topLeft"
+          title={"Are you sure you want to delete this dish?"}
+          onConfirm={() => deleteDish(item)}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button danger>Remove</Button>
+        </Popconfirm>
+      ),
+    },
   ];
 
   const renderNewItemButton = () => (
-    <Link to={'create'}>
+    <Link to={"create"}>
       <Button type="primary">New Item</Button>
     </Link>
-  )
+  );
 
   return (
     <Card title={"Menu"} style={{ margin: 20 }} extra={renderNewItemButton()}>
